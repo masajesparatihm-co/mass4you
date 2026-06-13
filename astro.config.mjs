0a0b8f8020de4dd696cf1c14efd1d98fa98a7393
Masajes Para Ti HM <masajes.para.ti.hm@gmail.com>
2026-06-13 13:35:18 -0500

    Final structure and forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Reemplaza 'tu-usuario' por tu nombre de GitHub
+  // y 'tu-repositorio' por el nombre que le darás a este proyecto
+  site: 'https://masajesparatihm-co.github.io/tu-repositorio',
+});
